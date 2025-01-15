dd107eb0c5ebc02f6ca81f9b9dc540ac5a264246
Jotanore <jotanore@hotmail.com>
2025-01-15 15:47:16 +0100

    Import pokemons

---
+++ js/pokemon.js
@@ -0,0 +1,26 @@
+import pokemons from '../src/pokemon/pokedex.json' with { type: 'json'};
+
+
+const BULBASAUR = {
+    "id": 1,
+    "name": {
+      "english": "Bulbasaur",
+      "japanese": "フシギダネ",
+      "chinese": "妙蛙种子",
+      "french": "Bulbizarre"
+    },
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "base": {
+      "HP": 45,
+      "Attack": 49,
+      "Defense": 49,
+      "Sp. Attack": 65,
+      "Sp. Defense": 65,
+      "Speed": 45
+    }
+  };
+
+  console.log(pokemons);
