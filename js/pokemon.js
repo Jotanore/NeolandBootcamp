683e9cb77010293795a53bca70c1f1c6f8ab6edf
Jotanore <jotanore@hotmail.com>
2025-01-15 17:57:40 +0100

    small changes

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,26 +1,35 @@
-import pokemons from '../src/pokemon/pokedex.json' with { type: 'json'};
+import POKEMONS from '../src/pokemon/pokedex.json' with { type: 'json'};
 
 
-const BULBASAUR = {
-    "id": 1,
-    "name": {
-      "english": "Bulbasaur",
-      "japanese": "フシギダネ",
-      "chinese": "妙蛙种子",
-      "french": "Bulbizarre"
-    },
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "base": {
-      "HP": 45,
-      "Attack": 49,
-      "Defense": 49,
-      "Sp. Attack": 65,
-      "Sp. Defense": 65,
-      "Speed": 45
+  console.log(POKEMONS);
+
+  readPokemonsList();
+
+  function readPokemonsList(){
+    const LISTA = document.getElementsByClassName('pokegallery')[0];
+
+    for(let i = 0; i < POKEMONS.length; i++){
+        let liElement = document.createElement('li');
+        liElement.className = 'pokemon-card'
+
+        let h1Element = document.createElement('h1');
+        h1Element.innerText = POKEMONS[i].name.english;
+
+        let figElement = document.createElement('figure');
+        
+
+
+        let imgElement = document.createElement('img');
+        imgElement.src = `../src/pokemon/thumbnails/${String(i).padStart(3, '0')}.png`;
+
+        let figcaptionElement = document.createElement('figure');
+        figcaptionElement.innerText = 'Nº' + numeroImagen;
+
+        figElement.appendChild(imgElement);
+
+
+        liElement.appendChild(h1Element);
+        liElement.appendChild(imgElement);
+        LISTA.appendChild(liElement);
     }
-  };
-
-  console.log(pokemons);
+  }
